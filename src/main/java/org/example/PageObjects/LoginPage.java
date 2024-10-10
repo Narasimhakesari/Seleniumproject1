95abd927b690bf5fae933ce1f5a11b2849e20e93
Narasimhakesari <narasimhakesari26@gmail.com>
2024-10-10 20:43:24 +0530

    Hello

---
+++ src/main/java/org/example/PageObjects/LoginPage.java
@@ -0,0 +1,20 @@
+package org.example.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    WebDriver driver;
+    public LoginPage(WebDriver driver)
+    {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    @FindBy(xpath = "Narasimha")
+    WebElement name;
+
+}
---
+++ src/test/java/org/example/Tests/CommonTest.java
@@ -0,0 +1,16 @@
+package org.example.Tests;
+
+import org.example.PageObjects.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CommonTest {
+
+    public static void main(String[] args)
+    {
+        WebDriver driver;
+        driver = new ChromeDriver();
+        LoginPage loginPage = new LoginPage(driver);
+    }
+
+}
