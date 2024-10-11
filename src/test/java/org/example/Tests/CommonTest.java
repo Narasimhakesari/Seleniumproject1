ed24da0c409e1e2e4de11df956fb937c9b0446ca
Narasimhakesari <narasimhakesari26@gmail.com>
2024-10-11 20:48:43 +0530

    commiting new

--- src/test/java/org/example/Tests/CommonTest.java
+++ src/test/java/org/example/Tests/CommonTest.java
@@ -3,14 +3,21 @@ package org.example.Tests;
 import org.example.PageObjects.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class CommonTest {
 
-    public static void main(String[] args)
+    @Test
+    public  void tst1()
     {
         WebDriver driver;
         driver = new ChromeDriver();
         LoginPage loginPage = new LoginPage(driver);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.get("https://parabank.parasoft.com/parabank/index.htm");
 
 
     }
